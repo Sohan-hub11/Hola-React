37f42e5d030c47773285798c70d7ef6b5ff826bc
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-09-10 02:32:16 +0530

    Add files via upload

---
+++ first-react-app/App.jsx
@@ -0,0 +1,17 @@
+import Button from './Button';
+import Hello from './Hello';
+import Random from './Random';
+function App() {
+  return <div>
+    <h1>
+      Hello Welcome to ReactJs!
+    </h1>
+    <Hello></Hello>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Button></Button>
+  </div>
+}
+export default App;
