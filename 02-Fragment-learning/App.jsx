a85f60d47cb266f13ae7ca6a5c87a33f76f68fff
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-10-09 21:23:44 +0530

    Create App.jsx

---
+++ 02-Fragment-learning/App.jsx
@@ -0,0 +1,32 @@
+//import { Fragment } from "react";
+//Fragment use to remove unneccesary <div> .
+
+import "./App.css"; 
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  //let fooditems = []; 
+  let fooditems = ["Dal", "Green Salad", "Milk", "Green Vegetables", "Roti", "Ghee"];
+
+  //Tertiary Operator.
+    //{fooditems.length === 0 ? <h4>I am Still Hungry</h4> : null}
+
+  //Logical Operators.
+  return (
+    <> 
+      <h1>Healthy Foods</h1>
+      {fooditems.length === 0 && <h4>I am Hungry</h4>}
+       
+      <ul className="list-group">
+        {
+          fooditems.map((item) => (
+               <li key={item} className="list-group-item">{item}</li>
+          ))
+        }
+       
+      </ul>
+    </>
+  ); 
+}
+
+export default App;
