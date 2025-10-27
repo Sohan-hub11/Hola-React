162f46960db49d1a4ed38a7a91d8019baf9d57d8
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-10-27 21:38:58 +0530

    Add files via upload

---
+++ 09-Context API/components/AppName.jsx
@@ -0,0 +1,9 @@
+import styles from "./AppName.module.css";
+
+const AppName = () => {
+    return <div>
+        <h1 className={styles.heading}>TODO App</h1>
+    </div>
+}
+
+export default AppName;
---
+++ 09-Context API/components/AppTodo.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { TodoItemsContext } from "../store/todo-items-store";
+import { useContext } from "react";
+const AppTodo = () => {
+    const { addNewItems } = useContext(TodoItemsContext);
+    const [todoName, setTodoname] = useState([]);
+    const [dueDate, setDueDate] = useState([]);
+
+    const handleNameChange = (event) => {
+        setTodoname(event.target.value);
+    }
+    const handleDateChange = (event) => {
+        setDueDate(event.target.value);
+    }
+    const handleAddButtonClicked = () => {
+        addNewItems(todoName, dueDate);
+        setTodoname("");
+        setDueDate("");
+    }
+
+    return <div>
+        <div className="container text-center">
+            <div className="row int-row">
+                <div className="col-6">
+                    <input type="text"
+                        placeholder="Enter Todo here"
+                        onChange={handleNameChange}
+                        value={todoName}
+                    />
+                </div>
+                <div className="col-4">
+                    <input type="date"
+                        onChange={handleDateChange} value={dueDate} />
+                </div>
+                <div className="col-2">
+                    <button type="button" className="btn btn-success int-button"
+                        onClick={handleAddButtonClicked}>Add</button>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default AppTodo;
